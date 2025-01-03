1ba1cd9293f57e6d821bcb73a46345290afec3ed
slsyswift <mlsy.swift@gmail.com>
2025-01-03 15:19:23 +0700

    初始化

---
+++ Podfile
@@ -0,0 +1,9 @@
+platform :ios, '12.0'
+
+target 'TYSDK' do
+
+  use_frameworks!
+
+  pod 'Display', :path => './submodoules/Display'
+
+end
---
+++ TYSDK/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  TYSDK
+//
+//  Created by apple on 2025/1/3.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
